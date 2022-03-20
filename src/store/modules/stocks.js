12b3e184f3071d6035f16295832716cb89ee9933
Lucas do Nascimento Motoshima <lucas@ultraha.us>
2022-03-19 21:00:27 -0300

    including state stock[]

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -1,6 +1,9 @@
 import stocks from '../../data/stocks'
 
 export default {
+    state: {
+        stocks: []
+    },
     mutations: {
         setStocks(state, stocks) {
             state.stocks = stocks
